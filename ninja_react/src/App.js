2de95019728cc02bf4e836f605f3eaef5a4369bf
github username <github email address>
2020-05-19 21:21:47 -0400

    swapped folders

---
+++ ninja_react/src/App.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { isMobile } from 'react-device-detect';
+import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
